119309a25e7f641c5fb6049ddd707e245ede7f64
Dudu <115907714+Dudu-Passoni@users.noreply.github.com>
2023-12-18 11:46:43 -0300

    Add files via upload

--- LibraryManagementSystem/src/main/java/lib/com/Book.java
+++ LibraryManagementSystem/src/main/java/lib/com/Book.java
@@ -1,5 +1,7 @@
 package lib.com;
 
+import java.util.Scanner;
+
 public class Book {
     private String nome;
     private float preco;
@@ -14,6 +16,36 @@ public class Book {
             System.out.println("It was not possible to schedule this book.");
     }
 
+    public void status(){
+        System.out.println("Nome: " + nome
+                + "\nAutor: " + autor
+                + "\nPreço: " + preco
+                + "\nQuantidade em Estoque: " + quantidade
+                + "\n______________________________________________\n");
+    }
+
+    public void estocar(){
+        Scanner Est = new Scanner(System.in);
+        System.out.println("What you want to do?\n\n1- Add\n2- Remove\n3- Exit");
+        switch (Est.nextInt()){
+            case 1:
+                System.out.println("How many books do you wanna add? ");
+                    int aux1 = Est.nextInt();
+                setQuantidade(getQuantidade() + aux1);
+                System.out.println(aux1 + " was added to the library");
+                System.out.println("______________________________________________\n");
+                    break;
+            case 2:
+                System.out.println("How many books do you wanna remove? ");
+                    int aux2 = Est.nextInt();
+                setQuantidade(getQuantidade() - aux2);
+                System.out.println(aux2 + " was removed from the library");
+                System.out.println("______________________________________________\n");
+                    break;
+            default:
+                    break;
+        }
+    }
 
     public Book() {
     }
